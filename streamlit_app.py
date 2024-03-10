8e17d036696d232dcd327d4e59a3d7c3a4329ed8
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-03-10 11:52:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,39 @@
 import streamlit as st
-import requests
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-# Function to get weather data
-def get_weather_data(city_name):
-    api_key = "b5fb90812d2e71ecfedeee7880d05e40"
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        weather_description = data['weather'][0]['description']
-        temperature = data['main']['temp']
-        humidity = data['main']['humidity']
-        wind_speed = data['wind']['speed']
-        return weather_description, temperature, humidity, wind_speed
-    else:
-        return None
+def main():
+    st.title("Plot your graph with Graphify")
+    st.subheader('Solve your graph-based assignments with ease')
 
-# Streamlit app
-st.title("City Data Viewer")
+    # Input fields for x and y values
+    x_values = st.text_input("Enter x values (separated by comma):")
+    y_values = st.text_input("Enter y values (separated by comma):")
 
-city_name = st.text_input("Enter the city name:")
-if city_name:
-    weather_data = get_weather_data(city_name)
-    if weather_data:
-        st.write(f"Weather in {city_name}: {weather_data[0]}")
-        st.write(f"Temperature: {weather_data[1]}°C")
-        st.write(f"Humidity: {weather_data[2]}%")
-        st.write(f"Wind Speed: {weather_data[3]} m/s")
-    else:
-        st.write("City not found. Please enter a valid city name.")
+    # Convert input strings to lists of floats
+    x_values_list = [float(x.strip()) for x in x_values.split(',')] if x_values else []
+    y_values_list = [float(y.strip()) for y in y_values.split(',')] if y_values else []
+
+    if x_values_list and y_values_list:
+        data = {'X axis': x_values_list, 'Y axis': y_values_list}
+        df = pd.DataFrame(data) #dataframe is a streamlit component
+        st.write(df)
+
+    if st.button("Plot Graph"):
+        try:
+            # Plot the graph
+            fig, ax = plt.subplots(figsize=(8, 6))
+            ax.plot(x_values_list, y_values_list, 'o-', color='#2563eb', label="Data Points")
+            ax.set_xlabel("$x$ $axis$", fontsize=14)
+            ax.set_ylabel("$y$ $axis$", fontsize=14)
+            ax.set_title("Your Graph")
+            ax.grid(True)
+            ax.legend()
+            st.pyplot(fig)
+
+        except ValueError:
+            st.error("Please enter valid numerical values.")
+
+if __name__ == "__main__":
+    main()
